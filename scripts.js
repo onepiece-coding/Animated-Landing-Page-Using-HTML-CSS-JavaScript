8a66d2aea1cdb4ba66dc1aba71c9219d6fbefb0a
onepiece-coding <makclaaax@gmail.com>
2024-12-02 04:10:05 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Select HTML Elements:
+const toggleNavbarEl = document.querySelector(".toggle-navbar");
+const navbarEl = document.querySelector(".navbar");
+
+// Toggle Navbar On Click:
+toggleNavbarEl.addEventListener("click", function () {
+    if (navbarEl.classList.contains("show")) {
+        this.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+        navbarEl.classList.remove("show");
+    } else {
+        this.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+        navbarEl.classList.add("show");
+    }
+});
